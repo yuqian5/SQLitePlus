322dd74e2bf876013af78e0de3c987735f281011
yuqian5 <28016308+yuqian5@users.noreply.github.com>
2020-10-03 17:12:44 -0600

    oxygen updated

--- docs/html/search/functions_0.js
+++ docs/html/search/functions_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['add_5fbinding_25',['add_binding',['../class_s_q_l_i_t_e3___q_u_e_r_y.html#a6d99ebd73c718573e95c75a85cbedd08',1,'SQLITE3_QUERY::add_binding(const std::string &amp;str)'],['../class_s_q_l_i_t_e3___q_u_e_r_y.html#a57de72a97b352fe0c33b2116e680500b',1,'SQLITE3_QUERY::add_binding(STRING b, STRINGS ... bs)']]],
+  ['add_5fbinding_25',['add_binding',['../class_s_q_l_i_t_e3___q_u_e_r_y.html#a6d99ebd73c718573e95c75a85cbedd08',1,'SQLITE3_QUERY::add_binding(const std::string &amp;str)'],['../class_s_q_l_i_t_e3___q_u_e_r_y.html#a9cef59c3975b2a76b9e8aab6a2595487',1,'SQLITE3_QUERY::add_binding(const char *str)'],['../class_s_q_l_i_t_e3___q_u_e_r_y.html#aca9ac7a1527db20ed3a73d5d991d8a4d',1,'SQLITE3_QUERY::add_binding(char *str)'],['../class_s_q_l_i_t_e3___q_u_e_r_y.html#a57de72a97b352fe0c33b2116e680500b',1,'SQLITE3_QUERY::add_binding(STRING b, STRINGS ... bs)']]],
   ['add_5ffunction_26',['add_function',['../class_s_q_l_i_t_e3.html#abbcb6d492c91ce07f4f4ba5d62f40a36',1,'SQLITE3']]]
 ];
